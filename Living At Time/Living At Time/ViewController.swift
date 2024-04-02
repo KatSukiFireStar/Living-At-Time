1cd37a39b819bb1dd3a58720e4065260c2613a2e
Flavien Gonin <gonin.flavien@gmail.com>
2024-04-02 10:47:37 +0200

    Creation du projet Xcode

---
+++ Living At Time/Living At Time/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Living At Time
+//
+//  Created by Flavien Gonin on 02/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
