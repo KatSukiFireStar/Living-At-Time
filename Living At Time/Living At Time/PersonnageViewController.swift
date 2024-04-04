cccd35d09d561c092b0bfeedf34c1ed308c63315
Flavien Gonin <gonin.flavien@gmail.com>
2024-04-04 16:06:49 +0200

    Ajout d'un nouvelle fenetre pour les personnages

---
+++ Living At Time/Living At Time/PersonnageViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PersonnageViewController.swift
+//  Living At Time
+//
+//  Created by Flavien Gonin on 04/04/2024.
+//
+
+import UIKit
+
+class PersonnageViewController: UIViewController {
+
+    var mage : Bool = false
+    var paysan : Bool = false
+    var paysane : Bool = false
+    var marchand : Bool = false
+    var reine : Bool = false
+    var chevalier : Bool = false
+    var templier : Bool = false
+    var ninja : Bool = false
+    var moine : Bool = false
+    var courtisane : Bool = false
+    var pape : Bool = false
+    var cultiste : Bool = false
+    var princesse : Bool = false
+    var seigneur : Bool = false
+    var conseiller : Bool = false
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
